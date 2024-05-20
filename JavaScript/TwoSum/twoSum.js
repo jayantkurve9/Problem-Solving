8c86f120ad6edcc19b2634c9ab1fc258540a0c54
Jayant Kurve <jayantkurve9@gmail.com>
2024-05-20 16:08:03 +0530

    Add code approach.

--- JavaScript/TwoSum/twoSum.js
+++ JavaScript/TwoSum/twoSum.js
@@ -1,3 +1,12 @@
+/*
+Approach: Hash Map
+* Created Hash Map cacheData.
+* Using cacheData to store keys and values where key is difference of target and array element and value is array element itself
+* While iterating on next element, first check if that element exists as a key in our cacheData.
+* If element found means that we had previously calculated its difference with target and its stored in cache.
+* Returning cacheData value and element as a two sum pair
+* If element not found in cache, storing the element as value against key as difference of target and element
+*/
 const twoSum = (array, target) => {
     const cacheData = {};
 
